60cf6181994f1755986ddb4ec4afe8bd95740759
Truong Tuan Manh <tuanmanh.truong@vn.panasonic.com>
2016-05-04 17:21:32 +0700

    Code for trajectories generator

--- data_generator.py
+++ data_generator.py
@@ -1,3 +1,6 @@
+import random
+import numpy as np
+from lxml import etree
 ##
 ## @brief      { class_description }
 ##
@@ -16,15 +19,15 @@ class Parameter:
     ## @return     { description_of_the_return_value }
     ##
     def readParametersFromXml(self, filename):
-        import xml.etree.ElementTree as ET
+
           
         try:
-            tree = ET.parse(filename)
+            tree = etree.parse(filename)
             root = tree.getroot()
       
             self.velocity = float(root.find('velocity').text)
-            self.minDistance = int(root.find('num_objects').text)
-            self.numObjects = float(root.find('min_distance').text)
+            self.numObjects = int(root.find('num_objects').text)
+            self.minDistance = float(root.find('min_distance').text)
             self.numFrames = int(root.find('num_frames').text)
         except Exception, e:
             import sys
@@ -34,15 +37,55 @@ class Parameter:
 ##
 ## @brief      { function_description }
 ##
-## @param      params  The params
+## @param      params  The settings read from XML file
 ##
-## @return     { description_of_the_return_value }
+## @return     { List of objects position in 2D }
 ##
 def initializePoints(params):
     try:
-        return 0
+    
+        # We create a grid of 25 x 4 points, then randomly choose the points from these 100 points 
+        indices = random.sample(range(1, 100), params.numObjects)
+        print "List of indices: ", indices
+        
+        # Convert indices to 2d position
+        objPositions = np.zeros((len(indices), 2))
+        for i in range(len(indices)):
+            #convert to 2d index
+            y_index = indices[i] / 25
+            x_index = indices[i] % 25
+            objPositions[i, 0] = x_index * 1.0 * params.minDistance
+            objPositions[i, 1] = y_index * 1.0 * params.minDistance
+
+        print "List of positions: ", objPositions
+        return objPositions
     except Exception, e:
         raise e
+##
+## @brief      { function_description }
+##
+## @param      initPositions  The init positions
+## @param      params         The params
+##
+## @return     { description_of_the_return_value }
+##
+def generateTrajectories(initPositions, params):
+    try:
 
+        velocity = np.array([params.velocity, 0])
+        deltaTime = 1
+        trajectories = etree.Element("trajectories")
+        for j in range(params.numFrames):
+            frame = etree.SubElement(trajectories, "frame", id=str(j))
+            for i in range(len(initPositions)):
 
+                # Propagate position along time
+                nextPos = initPositions[i,:] + velocity * deltaTime * j
 
+                # if object inside the observation area then output as measurement
+                if (nextPos[0] >= params.minDistance * 25 and nextPos[0] < params.minDistance * 25 + 3 ):
+                    obj = etree.SubElement(frame, "obj", id=str(i), x=str(nextPos[0]), y=str(nextPos[1]))
+        tree = etree.ElementTree(trajectories)
+        tree.write("trajectories.XML", pretty_print=True)
+    except Exception, e:
+        raise e
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from data_generator import Parameter
-
+import data_generator
 if __name__ == "__main__":
     params = Parameter()
     params.readParametersFromXml('country.xml')
     print params.velocity, params.numObjects, params.minDistance
+    points = data_generator.initializePoints(params)
+    data_generator.generateTrajectories(points, params)
